3e5e83e5b2aa463ea8ee35bde726e1f0bbd285b5
shaily26 <shaily.nupur@gmail.com>
2021-08-24 21:31:53 +0200

    Commiting test.py

--- sparkAssignment/Test/test.py
+++ sparkAssignment/Test/test.py
@@ -1,33 +1,20 @@
 import unittest
-import etl
-from Dependencies.spark import start_spark, conf_read
+from pyspark.sql import SparkSession
 
 
 class SomeUnitTest(unittest.TestCase):
 
-        def test_column(self):
+    def test_column(self):
+        spark1 = SparkSession.builder.getOrCreate()
 
-            spark1, message_prefix1, logging1 = start_spark(app_name='my_etl_job')
+        df11 = spark1.read.csv("client_data/", header=True, inferSchema=True)
 
-            df11 = spark1.read.csv("client_data/", header=True, inferSchema=True)
+        result = df11.schema.names
 
-            result = df11.schema.names
+        column_name = ['client_identifier', 'email', 'country', 'bitcoin_address', 'credit_card_type']
 
-            column_name = ['client_identifier', 'email', 'country', 'bitcoin_address', 'credit_card_type']
-            self.assertEqual(result, column_name)
+        self.assertEqual(result, column_name)
 
 
 if __name__ == '__main__':
-
-    spark, message_prefix, logging = start_spark(app_name='my_etl_job')
-
-    config = conf_read()
-
-    df1, df2 = etl.read_file(spark, logging, config["dataset1"], config["dataset2"])
-
-    # taking name of the country as the filter from config file
-    df_output = etl.business_function(spark, logging, df1, df2, config["countryFilter"])
-
-    etl.save_output(logging, df_output, config["test_file"])
-
     unittest.main()
