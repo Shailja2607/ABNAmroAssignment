16526103f3aae8c00a0e233ba501e3c835ae91a6
shaily26 <shaily.nupur@gmail.com>
2021-08-24 21:46:05 +0200

    commiting install.sh and travis.yml

---
+++ sparkAssignment/install-spark.sh
@@ -0,0 +1,8 @@
+  
+#!/usr/bin/env bash
+set -e
+SPARK_VERSION='3.1.2'
+HADOOP_VERSION='3.2'
+
+curl http://apache.mirror.gtcomm.net/spark/spark-$SPARK_VERSION/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz --output /tmp/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz
+cd /tmp && tar -xvzf /tmp/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz
